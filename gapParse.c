13b560d92ac0cfdda254d930a9510413b6dcb1af
Edgar <edgano@gmail.com>
2019-10-30 17:40:06 +0100

    double way parsing TODO

--- gapParse.c
+++ gapParse.c
@@ -1,148 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-char *readFile(char *fileName)
-{
-    FILE *file = fopen(fileName, "r");
-    char *code;
-    size_t n = 0;
-    int c;
+bool gaps;
+
+void convert_final_digit(){
+
+}
+void convertGaps (char *line){
+    char *c;
     char gap = '-';
     int numberGap=0;
+    char nn; 
 
-    if (file == NULL)
-        return NULL; //could not open file
+    for (c=line; *c; c++) {
 
-    code = malloc(1000);
-    char texto[1000];
-    while ((c = fgetc(file)) != EOF)
-    {
-        // code[n++] = (char) c;
-        // printf("%c", c);
-        if (c == gap){
+        nn = *c;
+        if (nn == gap)  {            // gap found
             numberGap++;
-            //printf("Gap: %c --> NumberGap: %i\n", c, numberGap);
         }else{            
-            if (numberGap!=0){
-                sprintf(texto, "%d", numberGap);
-                strcat(code, texto);
-                //printf("::inside:: %s\n",code);
-                // code[n++]= texto;
-                // printf("Insert gap integer: %s <<-->> %s\n", texto,code);
+            if (numberGap!=0){      // 1st non-gap after gap
+                //print number of gaps
+                printf("%d", numberGap);
+                //reset counter
                 numberGap=0;
-                for (int i=1; i<=strlen(texto); i++){
-                    n=n+1;
-                }
             }
-            code[n++] = (char) c;
-            //printf("Char: %c\n", c);
+            //print nucleotide
+            printf("%c", nn);
         }
     }
-
-    // don't forget to terminate with the null character
-    code[n] = '\0';
-
-    //printf("There are %i gaps\n", numberGap);
-    printf("Final alignment: %s\n", code);
-    return code;
 }
-int printFile(char *filename){
-
-    FILE *fptr; 
-    char c;
-
-    // Open file 
-    fptr = fopen(filename, "r"); 
-    if (fptr == NULL) 
-    { 
-        printf("Cannot open file \n"); 
-        exit(0); 
-    } 
-  
-    // Read contents from file 
-    c = fgetc(fptr); 
-    while (c != EOF) 
-    { 
-        printf ("%c", c); 
-        c = fgetc(fptr); 
-    } 
-  
-    fclose(fptr); 
-    return 0; 
-}
-void convertFasta(char * file){
-    char *code;
-    size_t n = 0;
-    int c;
-    char gap = '-';
+void convertFasta (char *line){
+    char *c;
     int numberGap=0;
-    code = malloc(1000);
-    char texto[1000];
+    char nn; 
+    int check=0;
+    int auxDigit=0;
+    char *aux;
 
-    while ((c = file)!= EOF)
-    {
-        // code[n++] = (char) c;
-        // printf("%c", c);
-        if (c == gap){
-            numberGap++;
-            //printf("Gap: %c --> NumberGap: %i\n", c, numberGap);
-        }else{            
-            if (numberGap!=0){
-                sprintf(texto, "%d", numberGap);
-                strcat(code, texto);
-                //printf("::inside:: %s\n",code);
-                // code[n++]= texto;
-                printf("Insert gap integer: %s <<-->> %s\n", texto,code);
-                numberGap=0;
-                for (int i=1; i<=strlen(texto); i++){
-                    n=n+1;
-                }
+    for (c=line; *c; c++) {
+
+        nn = *c;
+        //printf("** what we have: %c\n",nn);
+
+        if (!isdigit(nn)){  //it's a nucleotide
+            printf("nucleotide: %c\n",nn);
+            if (check==0){  //digit already covnerted
+                //printf("%c",nn);    //print nn
+            }else{          //first time to nucelotide after digit
+                //printf("auxDigit: : : %i",auxDigit);
+                convert_final_digit();
+                check=0;
+                //printf("%c",nn);
             }
-            code[n++] = (char) c;
-            //printf("Char: %c\n", c);
+        }else{              //its a digit
+            printf(">> number: %c\n",nn);
+            //TODO -> append nn
+            //auxDigit= atoi(aux);  //apend digit to gapNumber
+            printf("<< digit %s",aux);
+            check=1;        //need to convert auxDigit to int
+        }   
+    }
+}
+
+void readFasta(char *fileName){
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+ 
+    fp = fopen(fileName, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+ 
+    int state = 0;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        /* Delete trailing newline */
+        if (line[read - 1] == '\n')
+            line[read - 1] = 0;
+        /* Handle comment lines*/
+        if (line[0] == '>') {
+            if (state == 1){
+                //printf("\n");
+            }
+            printf("%s: \n", line+1);
+            state = 1;
+        } else {
+            /* Print everything else */
+            if (gaps){  //fasta >> gaps
+                convertGaps(line);
+            }else {     // gaps >> fasta
+                convertFasta(line);
+            }
+            printf("\n");
+            //printf("%s\n", line);
         }
     }
+ 
+    fclose(fp);
+    if (line)
+        free(line);
+    //exit(EXIT_SUCCESS);
+}
 
+void convert2Fasta(char *filename){
+    gaps = false;
+    readFasta(filename);
 }
-int readFasta(char *filename)
-{
-	FILE * fp;
-	char * line = NULL;
-	size_t len = 0;
-	ssize_t read;
- 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
-		exit(EXIT_FAILURE);
- 
-	int state = 0;
-	while ((read = getline(&line, &len, fp)) != -1) {
-		/* Delete trailing newline */
-		if (line[read - 1] == '\n')
-			line[read - 1] = 0;
-		/* Handle comment lines*/
-		if (line[0] == '>') {
-			if (state == 1)
-				printf("\n");
-			printf("%s: ", line+1);
-			state = 1;
-		} else {
-			/* Print everything else */
-			printf("%s", line);
-            convertFasta(line);
-		}
-	}
-	printf("\n");
- 
-	fclose(fp);
-	if (line)
-		free(line);
-	exit(EXIT_SUCCESS);
+void convert2Gap(char *filename){
+    gaps = true;
+    readFasta(filename);
 }
-int main()
-{
-   readFile("test.fa");
-   printFile("test.fa");
-   return 0;
+
+
+
+int main(){
+    printf("## FASTA TO GAP ##\n\n");
+    convert2Gap("test.fa");
+
+    printf("\n## GAP TO FASTA ##\n\n");
+    convert2Fasta("test.gap");
+
+    printf("## ## ## ## ##\n\n");
+
+    return 0;
 }
